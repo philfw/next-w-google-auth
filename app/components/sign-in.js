a36e7355f23f6bfd2731c7ce58e341d0e7571edd
philfw <philfw@gmail.com>
2024-12-09 09:24:42 -0500

    user-image stopped working

--- app/components/sign-in.js
+++ app/components/sign-in.js
@@ -2,16 +2,14 @@
 
 import { signIn, signOut } from "next-auth/react"
 import { useSession } from "next-auth/react";
-import styles from "./SignIn.module.css";
  
 export default function SignIn() {
   const { data: session, status } = useSession()
 
   if (status === "authenticated") {
     return (
-      <div className={styles.container}>
-        <img src={session.user.image} className={styles.user}></img>
-        <p>Signed in as {session.user.name}</p>
+      <div>
+        <p>Signed in as {session.user.email}</p>
         <button onClick={() => signOut("google")}>Sign Out</button>
       </div>
     )
